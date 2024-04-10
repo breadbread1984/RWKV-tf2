7200c65561b3d0f788b5bc88c6690a11e61de032
breadbread1984 <breadbread1984@163.com>
2024-04-10 17:50:19 +0800

    update

--- models.py
+++ models.py
@@ -48,7 +48,7 @@ class RWKVAttention(tf.keras.layers.Layer):
     x = hidden # x.shape = (batch, seq_len, hidden)
     shifted_x = tf.concat([tf.expand_dims(attn_x, axis = 1), x[:,0:-1,:]], axis = 1) # shifted_x.shape = (batch, seq_len, hidden)
     time_mix = x + (shifted_x - x) * self.time_maa_x # xxx.shape = (batch, seq_len, hidden)
-    # NOTE: W2 @ W1 @ (mu * x_t + (1 - mu) * x_{t-1}), equations (11)-(15)
+    # NOTE: W2 @ tanh (W1 @ (mu * x_t + (1 - mu) * x_{t-1})), equations (11)-(15)
     xxx = tf.transpose(tf.reshape(tf.math.tanh(tf.linalg.matmul(time_mix, self.time_maa_w1)), (-1, 5, 32)), (1,0,2)) # xxx.shape = (5, batch * seq_len, 32)
     xxx = tf.reshape(tf.linalg.matmul(xxx, self.time_maa_w2), (5, tf.shape(hidden)[0], tf.shape(hidden)[1], self.hidden_size)) # xxx.shape = (5, batch, seq_len, hidden_size)
     mw, mk, mv, mr, mg = xxx[0,...], xxx[1,...], xxx[2,...], xxx[3,...], xxx[4,...] # shape = (batch, seq_len, hidden_size)
