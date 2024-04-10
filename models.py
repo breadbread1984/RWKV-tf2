12af558c6e80d9e7de84098518684b895d800ab5
breadbread1984 <breadbread1984@163.com>
2024-04-10 10:24:41 +0800

    update

--- models.py
+++ models.py
@@ -39,6 +39,7 @@ class RWKVAttention(tf.keras.layers.Layer):
     # ffn_x.shape = (batch, hidden)
 
     # 1) linear to get key value
+    # NOTE: time mix first time
     # NOTE: time_mix = mu * shifted_x + (1 - mu) * x = mu * x_t + (1 - mu) * x_{t-1}
     x = hidden # x.shape = (batch, seq_len, hidden)
     shifted_x = tf.concat([tf.expand_dims(attn_x, axis = 1), x[:,0:-1,:]], axis = 1) # shifted_x.shape = (batch, seq_len, hidden)
@@ -47,7 +48,8 @@ class RWKVAttention(tf.keras.layers.Layer):
     xxx = tf.transpose(tf.reshape(tf.math.tanh(tf.linalg.matmul(time_mix, self.time_maa_w1)), (-1, 5, 32)), (1,0,2)) # xxx.shape = (5, batch * seq_len, 32)
     xxx = tf.reshape(tf.linalg.matmul(xxx, self.time_maa_w2), (5, tf.shape(hidden)[0], tf.shape(hidden)[1], self.hidden_size)) # xxx.shape = (5, batch, seq_len, hidden_size)
     mw, mk, mv, mr, mg = xxx[0,...], xxx[1,...], xxx[2,...], xxx[3,...], xxx[4,...] # shape = (batch, seq_len, hidden_size)
-
+    # NOTE: time mix second time
+    # NOTE: value = (w + m) * shifted_x + (1 - (w + m)) * x
     time_decay = x + (shifted_x - x) * (self.time_maa_w + mw) # time_decay.shape = (batch, seq_len, hidden)
     key = x + (shifted_x - x) * (self.time_maa_k + mk) # key.shape = (batch, seq_len, hidden)
     value = x + (shifted_x - x) * (self.time_maa_v + mv) # value.shape = (batch, seq_len, hidden)
